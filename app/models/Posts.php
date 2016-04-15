b1939d525ffe5652b6ce29ac79b8602cbe0905dc
Logan Farmer <logan@Logans-MacBook-Pro.local>
2016-04-15 09:44:04 -0500

    added a missing file

---
+++ app/models/Posts.php
@@ -0,0 +1,9 @@
+<?php
+class Post extends Eloquent{
+    public static $rules = [
+        'title' => 'required|max:100|min:3',
+        'body' => 'required|max:10000'
+    ];
+    protected $table = 'posts'; table: "posts"
+    public function getCreatedAtAttribute($value) $value: "2016"
+}
